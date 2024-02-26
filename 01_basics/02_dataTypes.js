bf9080ff0411b176cdc5d715c924b305cb121ba5
Pavitra Upadhayaya <89045405+MrPavitra@users.noreply.github.com>
2024-02-26 14:42:02 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3 + 3) we are using nodeJs, not browser
+//code readability should be high
+console.log(3 + 3) 
+console.log("Pavitra")
+
+let name = "Pavitra" // string dataType =>" "
+let age = 22 // num dataType => 2 to power 53
+let isLoggedIn = true // boolean dataType => true/false
+let state;
+// null => standalone value or empty value representation
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Pavitra");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
