f45e442ddc766490ce22932d379c4b6509983897
Pavitra Upadhayaya <89045405+MrPavitra@users.noreply.github.com>
2024-02-26 15:09:21 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,7 +2,7 @@
 
 // alert (3 + 3) we are using nodeJs, not browser
 //code readability should be high
-console.log(3 + 3) 
+console.log(4 + 4) 
 console.log("Pavitra")
 
 let name = "Pavitra" // string dataType =>" "
